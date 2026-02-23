987641e4c3936379a751c033d2a4156893c12646
BlathaBlather <michael.henson@delinear.eu>
2026-02-23 15:32:53 +0000

    Add GPIO client instances for gpio2, gpio3, and gpio4

---
+++ gpio/clients.ts
@@ -0,0 +1,8 @@
+namespace modules {
+    //% fixedInstance whenUsed weight=2 block="gpio2"
+    export const gpio2 = new GPIOClient("gpio2")
+    //% fixedInstance whenUsed weight=3 block="gpio3"
+    export const gpio3 = new GPIOClient("gpio3")
+    //% fixedInstance whenUsed weight=4 block="gpio4"
+    export const gpio4 = new GPIOClient("gpio4")
+}
